d24248a0c9677fa1467069cb60bac8dcb0f3e76e
alen9968 <xwzj0608@qq.com>
2020-03-25 18:27:50 +0800

    fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React, { Component, useEffect, useState } from "react";
-import { Input, Button, Row, Col } from 'antd'
+import { Input, message, Button, Row, Col } from 'antd'
 import { useDispatch, useSelector  } from 'react-redux'
 import { viewMergeIn,viewSetIn   } from '../redux/modules/view'
 import { articlesSet } from '../redux/modules/articles'
@@ -11,26 +11,27 @@ import { Skeleton  } from 'antd';
 
 
 const Home = () => {
-  // const dispatch = useDispatch()
-  // const view = useSelector(state => state.view)
-  // const message = view.get('message')
-  // console.log(message)
+  const dispatch = useDispatch()
+  const articles = useSelector(state => state.articles)
 
-  //const [chatHistory, setChatHistory] = useState([]);
   //const [message, setMessage] = useState('');
-  const articlesIndex = async() => {
+
+  const getArticles = async() => {
     const res = await request('get', 'articles')
-    console.log("res:.data", res.data)
-    console.log("res:", res)
+    if (!res.data){
+      dispatch(articlesSet(res.data))
+    } else{
+      //message.warn('数据请求错误！')
+    }
   }
 
   useEffect(() => {
-    articlesIndex()
-  }, [])
+    getArticles()
+  }, [getArticles])
 
   return (
     <div style={{ }}>
-        <Row style={{  }}>
+      <Row style={{  }}>
         Homomeshhhhhomeshhhhhomeshhhhhomeshhhhhomeshhhhhomeshhhhheshhhhhomeshhhhhh
       </Row>
     </div>
